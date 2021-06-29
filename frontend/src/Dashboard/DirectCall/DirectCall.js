54b81a1c54110cc4fc1b7043a43e3de5d9952d1a
Amogh24 <55030597+Amogh24@users.noreply.github.com>
2021-06-29 11:50:35 +0530

    incoming call message

--- frontend/src/Dashboard/DirectCall/DirectCall.js
+++ frontend/src/Dashboard/DirectCall/DirectCall.js
@@ -4,7 +4,7 @@ import LocalVideoView from '../LocalVid/LocalVideoView';
 import RemoteVideoView from '../RemoteVid/RemoteVideoView';
 import CallingMessage from '../CallingMessage/CallingMessage';
 import CallRejectedMessage from '../CallRejected/CallRejectedMessage';
-
+import IncomingCallMessage from '../IncomingCallMessage/IncomingCallMessage';
 const DirectCall = (props) => {
   const { localStream, remoteStream } = props;
 
@@ -13,7 +13,8 @@ const DirectCall = (props) => {
       <LocalVideoView localStream={localStream} />
       {remoteStream && <RemoteVideoView remoteStream={remoteStream} />}
       {/* <CallingMessage/> */}
-      <CallRejectedMessage/>
+      {/* <CallRejectedMessage/> */}
+      <IncomingCallMessage/>
     </>
   );
 };
