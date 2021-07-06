93b3280a76f63e15f133578b5bb9a47d926426ce
Amogh24 <55030597+Amogh24@users.noreply.github.com>
2021-07-06 16:44:24 +0530

    creating connections for group calls

--- frontend/src/utils/Webrtc/webRTCGroupCallHandler.js
+++ frontend/src/utils/Webrtc/webRTCGroupCallHandler.js
@@ -18,6 +18,12 @@ export const connectWithMyPeer = ()=>{
       console.log(id);
       myPeerId = id;
   })
+  myPeer.on('call',call=>{
+    call.answer(store.getState().call.localStream)
+    call.on('stream',incomingStream=>{
+      console.log("stream arrived")
+    })
+  })
 }
 
 export const createNewGroupCall = () =>{
@@ -42,5 +48,10 @@ export const joinGroupCall = (hostSocketId,roomId)=>{  //function which calls in
 }
 
 export const connectToNewUser = (data)=>{
-  //hello
+  //function to add user to group call
+  const localStream = store.getState().call.localStream   //fetching localstream from the store
+  const call = myPeer.call(data.peerId,localStream);  //Calls the remote peer specified by id and returns a media connection.
+  call.on('stream',(incomingStream)=>{
+    console.log('stream came');
+  })
 }
