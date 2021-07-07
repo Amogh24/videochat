b2512cbec89a88bc91b48a6ec047ae54bc0c4efb
Amogh24 <55030597+Amogh24@users.noreply.github.com>
2021-07-07 09:54:59 +0530

    added background to group call video

--- frontend/src/Dashboard/GroupCallRoom/GroupCallVideo.js
+++ frontend/src/Dashboard/GroupCallRoom/GroupCallVideo.js
@@ -3,11 +3,14 @@ import React, { useRef, useEffect } from 'react';
 const styles = {
   videoContainer: {
     width: '300px',
-    height: '300px'
+    height: '300px',
+    paddingLeft:'10px'
   },
   videoElement: {
     width: '100%',
-    height: '100%'
+    height: '100%',
+    borderRadius:"8px",
+    backgroundColor:'white'
   }
 };
 
