a0968cf66a048c7e573cc8290a4cc14f38566aca
Amogh24 <55030597+Amogh24@users.noreply.github.com>
2021-07-11 09:15:00 +0530

    changed server path in wss for deployment

--- frontend/src/utils/wssConnection/wssConnection.js
+++ frontend/src/utils/wssConnection/wssConnection.js
@@ -4,7 +4,7 @@ import * as dashboardActions from '../../store/actions/dashboardActions'
 import * as webRTChandler from '../Webrtc/WebrtcHandler'
 import * as webRTCGroupCallHandler from '../Webrtc/webRTCGroupCallHandler'
 
-const SERVER = 'http://localhost:5000'
+const SERVER = 'https://vconnect-backend.herokuapp.com/'
 
 let socket;
 
